d504610e5fc38fc9a0318d280b06b4fae1b830db
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-02 15:30:30 -0300

    :sparkles: feature: finished component

--- src/components/MediaMatch/index.tsx
+++ src/components/MediaMatch/index.tsx
@@ -1,11 +1,28 @@
-import * as S from "./MediaMatch.styles";
+import styled, { css } from "styled-components";
+import media, { DefaultBreakpoints } from "styled-media-query";
 
-function MediaMatch() {
-  return (
-    <S.Wrapper>
-      <h1>MediaMatch</h1>
-    </S.Wrapper>
-  );
-}
+type breakpoint = keyof DefaultBreakpoints;
+
+export type MediaMatchProps = {
+  lessThan?: breakpoint;
+  greaterThan?: breakpoint;
+};
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoint) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+  greaterThan: (size: breakpoint) => css`
+    ${media.greaterThan(size)`display: block`}
+  `,
+};
+
+const MediaMatch = styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`;
 
 export default MediaMatch;
