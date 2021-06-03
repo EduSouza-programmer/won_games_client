65d4a379ffa3056b678d08287fbbb1d0cfec0f6f
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-03 09:52:36 -0300

    :sparkles: feature: Starting the <Menu/> compon…
    Closes #9

---
+++ src/components/Menu/Menu.test.tsx
@@ -0,0 +1,14 @@
+import { screen } from "@testing-library/react";
+import { renderWithTheme } from "@/utils/tests/helpers";
+
+import Menu from ".";
+
+describe("<Menu />", () => {
+  it("should render the heading", () => {
+    const { container } = renderWithTheme(<Menu />);
+
+    expect(screen.getByRole("heading", { name: /Menu/i })).toBeInTheDocument();
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./Menu.styles";
+
+function Menu() {
+  return (
+    <S.Wrapper>
+      <h1>Menu</h1>
+    </S.Wrapper>
+  );
+}
+
+export default Menu;
