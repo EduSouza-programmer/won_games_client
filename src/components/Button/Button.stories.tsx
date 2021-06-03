f5ab37f5f7605d6c95a9b64ae6813f37e2430a74
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-02 21:58:11 -0300

    :pencil: docs: storybook component <Button/>

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,9 +1,33 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import Button from ".";
+import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart";
+
+import Button, { ButtonProps } from ".";
 
 export default {
   title: "Button",
   component: Button,
+  argTypes: {
+    children: {
+      type: "string",
+      defaultValue: "Buy now",
+    },
+    size: {
+      options: ["small", "medium", "large"],
+      control: { type: "select" },
+      defaultValue: "medium",
+    },
+    icon: {
+      type: "",
+    },
+  },
 } as Meta;
 
-export const Default: Story = () => <Button />;
+export const Default: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const WithIcon: Story<ButtonProps> = (args) => <Button {...args} />;
+
+WithIcon.args = {
+  size: "small",
+  children: "Buy Now",
+  icon: <AddShoppingCart />,
+};
