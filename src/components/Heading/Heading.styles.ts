cb4570aa57b1eb8c33c735fbdd64713c3ab436a0
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-04 12:58:43 -0300

    :recycle: refactoring: finished refactory component …

--- src/components/Heading/Heading.styles.ts
+++ src/components/Heading/Heading.styles.ts
@@ -2,39 +2,51 @@ import styled, { css, DefaultTheme } from "styled-components";
 
 import media from "styled-media-query";
 
-import { HeadingProps } from ".";
+import { HeadingProps, LineColor } from ".";
 
 const wrapperModifiers = {
-  lineLeft: (theme: DefaultTheme) => css`
+  lineLeft: (theme: DefaultTheme, lineColor: LineColor) => css`
     padding-left: ${theme.spacings.xxsmall};
-    border-left: 0.7rem solid ${theme.palette.secondary.main};
+    border-left: 0.5rem solid ${theme.palette[lineColor!].main};
   `,
 
-  lineBottom: (theme: DefaultTheme) => css`
+  lineBottom: (theme: DefaultTheme, lineColor: LineColor) => css`
     position: relative;
 
     &::after {
       position: absolute;
       left: 0;
-      bottom: -1rem;
+      bottom: -0.7rem;
       content: "";
       width: 5rem;
-      border-top: 0.7rem solid ${theme.palette.primary.main};
+      border-top: 0.5rem solid ${theme.palette[lineColor!].main};
     }
+  `,
+
+  small: (theme: DefaultTheme) => css`
+    font-size: ${theme.typography.fontSizes.medium};
+
+    ::after {
+      width: 3rem;
+    }
+  `,
+
+  medium: (theme: DefaultTheme) => css`
+    font-size: ${theme.typography.fontSizes.xlarge};
+
+    ${media.greaterThan("medium")`
+      font-size: ${theme.typography.fontSizes.xxlarge};
+
+`}
   `,
 };
 
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color, lineLeft, lineBottom }) => css`
+  ${({ theme, color, lineLeft, lineBottom, size, lineColor }) => css`
     color: ${theme.palette[color!].main};
-    font-size: ${theme.typography.fontSizes.xlarge};
 
-    ${media.greaterThan("medium")`
-    font-size: ${theme.typography.fontSizes.xxlarge};
-
-    `}
-
-    ${lineLeft && wrapperModifiers.lineLeft(theme)};
-    ${lineBottom && wrapperModifiers.lineBottom(theme)};
+    ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor)}
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `;
