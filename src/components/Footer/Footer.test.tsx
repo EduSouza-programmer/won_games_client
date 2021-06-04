aabfcc7c00d603ab110913ee032a1adc2380e713
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-04 19:46:48 -0300

    :white_check_mark: test: adding tests component <F…

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -4,13 +4,18 @@ import { renderWithTheme } from "@/utils/tests/helpers";
 import Footer from ".";
 
 describe("<Footer />", () => {
-  it("should render the heading", () => {
-    const { container } = renderWithTheme(<Footer />);
+  it("should render 4 coloumns topics", () => {
+    renderWithTheme(<Footer />);
 
     expect(
-      screen.getByRole("heading", { name: /Footer/i })
+      screen.getByRole("heading", { name: /contact/i })
     ).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
+    expect(
+      screen.getByRole("heading", { name: /follow us/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: /links/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /location/i })
+    ).toBeInTheDocument();
   });
 });
