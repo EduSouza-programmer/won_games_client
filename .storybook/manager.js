a9ac6bf0cefa43b9b961876c0cc6030ebd49a929
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-04 12:56:36 -0300

    :wrench: config: create theme for storybook

---
+++ .storybook/manager.js
@@ -0,0 +1,20 @@
+import { addons } from '@storybook/addons';
+import myTheme from './myTheme'
+
+
+
+addons.setConfig({
+  isFullscreen: false,
+  showNav: true,
+  showPanel: true,
+  panelPosition: 'right',
+  enableShortcuts: true,
+  isToolshown: true,
+  theme: myTheme,
+  selectedPanel: undefined,
+  initialActive: 'sidebar',
+  sidebar: {
+    showRoots: false,
+    collapsedRoots: ['other'],
+  },
+});
