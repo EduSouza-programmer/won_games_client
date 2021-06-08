d86f1c8d132e61664bb427b4fb44883538f0fca5
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-08 17:54:29 -0300

    :pencil: docs: created storybook the component <Hi…

--- src/components/Highlight/Highlight.stories.tsx
+++ src/components/Highlight/Highlight.stories.tsx
@@ -1,9 +1,61 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import Highlight from ".";
+import Highlight, { HighlightProps } from ".";
 
 export default {
   title: "Highlight",
   component: Highlight,
-} as Meta;
+  argTypes: {
+    alignment: {
+      options: ["left", "right"],
+      control: { type: "select" },
+    },
+  },
+  args: {
+    title: "Read Dead it's back",
+    subtitle: "Come see John's new adventures",
+    buttonLabel: "Buy now",
+    buttonLink: "https://any",
+    backgroundImage: "/img/red-dead-img.jpg",
+  },
+  parameters: {
+    layout: "fullscreen",
+  },
+} as Meta<HighlightProps>;
 
-export const Default: Story = () => <Highlight />;
+export const Mobile: Story<HighlightProps> = (args) => <Highlight {...args} />;
+
+Mobile.parameters = {
+  viewport: {
+    defaultViewport: "mobile1",
+  },
+};
+
+export const MobileWithFloatImage: Story<HighlightProps> = (args) => (
+  <Highlight {...args} />
+);
+
+MobileWithFloatImage.parameters = {
+  viewport: {
+    defaultViewport: "mobile1",
+  },
+};
+
+MobileWithFloatImage.args = {
+  floatImage: "/img/red-dead-float.png",
+};
+
+export const Desktop: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: "104rem", margin: "0 auto" }}>
+    <Highlight {...args} />
+  </div>
+);
+
+export const DesktopWithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: "104rem", margin: "0 auto" }}>
+    <Highlight {...args} />
+  </div>
+);
+
+DesktopWithFloatImage.args = {
+  floatImage: "/img/red-dead-float.png",
+};
