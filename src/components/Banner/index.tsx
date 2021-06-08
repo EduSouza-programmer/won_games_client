b6b422aa3d85525a24ad7d44f2979b686ce92bf0
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-08 13:59:55 -0300

    :sparkles: feature: created the component <Banner/>…

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,9 +1,43 @@
+import Ribbon, { RibbonSizes, RibbonColors } from "@/components/Ribbon";
+import Button from "@/components/Button";
 import * as S from "./Banner.styles";
 
-function Banner() {
+export type BannerProps = {
+  img: string;
+  title: string;
+  subtitle: string;
+  buttonLabel: string;
+  buttonLink: string;
+  ribbon?: React.ReactNode;
+  ribbonSize?: RibbonSizes;
+  ribbonColor?: RibbonColors;
+};
+
+function Banner({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink,
+  ribbon,
+  ribbonColor = "primary",
+  ribbonSize = "normal",
+}: BannerProps) {
   return (
     <S.Wrapper>
-      <h1>Banner</h1>
+      <S.Image src={img} role="img" aria-label={title} />
+      <S.Caption>
+        <S.Title>{title}</S.Title>
+        <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+        <Button as="a" href={buttonLink} size="large">
+          {buttonLabel}
+        </Button>
+      </S.Caption>
+      {!!ribbon && (
+        <Ribbon size={ribbonSize} color={ribbonColor}>
+          {ribbon}
+        </Ribbon>
+      )}
     </S.Wrapper>
   );
 }
