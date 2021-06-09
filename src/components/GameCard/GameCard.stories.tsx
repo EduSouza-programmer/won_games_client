c08e2ae78c514125b7e530ba37657d9d448339eb
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-09 10:45:11 -0300

    :fire: prune: revert commit

--- src/components/GameCard/GameCard.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from "@storybook/react/types-6-0";
-import GameCard from ".";
-
-export default {
-  title: "GameCard",
-  component: GameCard,
-} as Meta;
-
-export const Default: Story = () => <GameCard />;
