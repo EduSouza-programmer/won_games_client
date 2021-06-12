57471791ca7e638f1e47e583dc88bef02bac24c2
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-12 09:26:27 -0300

    :sparkles: feature: created the component <GameCardSlider/>

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -1,9 +1,61 @@
+import { ArrowBackIos as ArrowLeft } from "@styled-icons/material-outlined/ArrowBackIos";
+import { ArrowForwardIos as ArrowRight } from "@styled-icons/material-outlined/ArrowForwardIos";
+
+import GameCard, { GameCardProps } from "@/components/GameCard";
+import Slider, { SliderSettings } from "@/components/Slider";
 import * as S from "./GameCardSlider.styles";
 
-function GameCardSlider() {
+const settings: SliderSettings = {
+  slidesToShow: 4,
+  infinite: false,
+  lazyLoad: "ondemand",
+  nextArrow: <ArrowRight aria-label="next games" />,
+  prevArrow: <ArrowLeft aria-label="previous games" />,
+  responsive: [
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1.1,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1.2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2.2,
+        arrows: false,
+      },
+    },
+    {
+      breakpoint: 1375,
+      settings: {
+        slidesToShow: 3.2,
+        arrows: false,
+      },
+    },
+  ],
+};
+
+export type GameCardSliderProps = {
+  items: GameCardProps[];
+  arrowColor?: "white" | "black";
+};
+
+function GameCardSlider({ items, arrowColor = "white" }: GameCardSliderProps) {
   return (
-    <S.Wrapper>
-      <h1>GameCardSlider</h1>
+    <S.Wrapper arrowColor={arrowColor}>
+      <Slider settings={settings}>
+        {items.map((item) => (
+          <GameCard key={item.title} {...item} />
+        ))}
+      </Slider>
     </S.Wrapper>
   );
 }
