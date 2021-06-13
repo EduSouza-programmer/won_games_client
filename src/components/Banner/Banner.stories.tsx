6607f7dd2a51a327dffd0de35375ea33ad0d1fb8
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-12 22:47:56 -0300

    :recycle: refactoring: finished storybook update <Banner/>

--- src/components/Banner/Banner.stories.tsx
+++ src/components/Banner/Banner.stories.tsx
@@ -18,33 +18,61 @@ export default {
     },
   },
   args: {
-    img: "https://images.unsplash.com/photo-1562229125-6d6075419a22?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80",
+    img: "img/banner-desktop.png",
     title: "Defy death",
     subtitle: "<p>Play the new <strong>CrashLands</strong> season",
     buttonLabel: "Buy now",
     buttonLink: "/games/defy-death",
   },
   parameters: {
-    layout: "fullscreen",
+    layout: "centered",
+    backgrounds: {
+      default: "dark",
+    },
   },
 } as Meta<BannerProps>;
 
-export const Mobile: Story<BannerProps> = (args) => <Banner {...args} />;
+export const Mobile: Story<BannerProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    }}
+  >
+    <Banner {...args} />
+  </div>
+);
 
 Mobile.parameters = {
   viewport: {
-    defaultViewport: "mobile1",
+    defaultViewport: "figmaMobile",
   },
+  layout: "fullscreen",
 };
 
 export const MobileWithRibbon: Story<BannerProps> = (args) => (
-  <Banner {...args} />
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      padding: "15px",
+    }}
+  >
+    <Banner {...args} />
+  </div>
 );
 
 MobileWithRibbon.parameters = {
   viewport: {
-    defaultViewport: "mobile1",
+    defaultViewport: "figmaMobile",
   },
+  layout: "fullscreen",
 };
 
 MobileWithRibbon.args = {
