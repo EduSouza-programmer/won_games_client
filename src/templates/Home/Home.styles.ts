93f94dce3267af40da5e008b91c1df15d4ea4c7b
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-15 15:35:39 -0300

    :sparkles: feature: created the page <Home/> templates

--- src/templates/Home/Home.styles.ts
+++ src/templates/Home/Home.styles.ts
@@ -1,7 +1,94 @@
+import * as HeadingStyles from "@/components/Heading/Heading.styles";
+import * as HighlightStyles from "@/components/Highlight/Highlight.styles";
+import * as GameCardSliderStyles from "@/components/GameCardSlider/GameCardSlider.styles";
 import styled, { css } from "styled-components";
+import media from "styled-media-query";
 
-export const Wrapper = styled.div`
+const Sections = styled.section`
   ${({ theme }) => css`
-    background-color: ${theme.palette.primary.main};
+    margin-bottom: calc(${theme.spacings.large} * 2);
+
+    ${HeadingStyles.Wrapper},
+    ${HighlightStyles.Wrapper},
+    ${GameCardSliderStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    ${HighlightStyles.Wrapper} {
+      ${media.lessThan("medium")`
+        margin-right: calc((${theme.gridLayout.gutter} / 2) * -1);
+        margin-left: calc((${theme.gridLayout.gutter} / 2) * -1);
+      `}
+    }
+
+    ${GameCardSliderStyles.Wrapper} {
+      ${media.lessThan("huge")`
+        margin-right: calc((${theme.gridLayout.gutter} / 2) * -1);
+      `}
+    }
+  `}
+`;
+
+export const SectionBanner = styled.section`
+  ${({ theme }) => css`
+    margin: ${theme.spacings.large} calc((${theme.gridLayout.gutter} / 2) * -1);
+
+    ${media.greaterThan("medium")`
+
+      margin: ${theme.spacings.large} 0;
+      position: relative;
+      z-index: ${theme.zIndex.base};
+
+    `}
+  `}
+`;
+
+export const SectionNews = styled(Sections)`
+  ${({ theme }) => css`
+    margin-bottom: calc(${theme.spacings.xxlarge} * 2);
+
+    ${media.greaterThan("medium")`
+      margin-bottom: 0;
+      padding-top: 15rem;
+      padding-bottom: 12rem;
+      background-color: ${theme.palette.lightBg.main};
+      clip-path: polygon(0 0, 100% 15%, 100% 100%, 0 85%);
+
+
+      ${HeadingStyles.Wrapper} {
+        color: ${theme.palette.black.main}
+      }
+    `}
+
+    ${media.greaterThan("large")`
+       margin-top: -13rem;
+    `}
+  `}
+`;
+
+export const SectionMostPopular = styled(Sections)``;
+
+export const SectionUpcomming = styled(Sections)`
+  ${({ theme }) => css`
+    ${HighlightStyles.Wrapper} {
+      margin-top: calc(${theme.spacings.xlarge} * 2);
+    }
+  `}
+`;
+export const SectionFreeGames = styled(Sections)``;
+
+export const SectionFooter = styled(Sections)`
+  ${({ theme }) => css`
+    margin-bottom: 0;
+    margin-top: ${theme.spacings.large};
+    padding-bottom: ${theme.spacings.xsmall};
+    padding-top: ${theme.spacings.xxlarge};
+    background-color: ${theme.palette.white.main};
+    clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
+
+    ${media.greaterThan("medium")`
+      padding-top: calc(${theme.spacings.xxlarge} * 2);
+      clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
+    `}
   `}
 `;
--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,10 +1,90 @@
+import Heading from "@/components/Heading";
+import Footer from "@/components/Footer";
+import Menu from "@/components/Menu";
+import BannerSlider from "@/components/BannerSlider";
+import GameCardSlider from "@/components/GameCardSlider";
+import { Container } from "@/components/Container";
+
+import { BannerProps } from "@/components/Banner";
+import { GameCardProps } from "@/components/GameCard";
+import Highlight, { HighlightProps } from "@/components/Highlight";
+
 import * as S from "./Home.styles";
 
-function Home() {
+export type HomeTamplateProps = {
+  banners: BannerProps[];
+  newGames: GameCardProps[];
+  mostPopularHighlight: HighlightProps;
+  mostPopularGames: GameCardProps[];
+  upcommingHighlight: HighlightProps;
+  upcommingGames: GameCardProps[];
+  upcommingMoreGames: GameCardProps[];
+  freeHighlight: HighlightProps;
+  freeGames: GameCardProps[];
+};
+
+function Home({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingHighlight,
+  upcommingGames,
+  upcommingMoreGames,
+  freeHighlight,
+  freeGames,
+}: HomeTamplateProps) {
   return (
-    <S.Wrapper>
-      <h1>Home</h1>
-    </S.Wrapper>
+    <section>
+      <Container>
+        <Menu />
+        <S.SectionBanner>
+          <BannerSlider items={banners} />
+        </S.SectionBanner>
+      </Container>
+
+      <S.SectionNews>
+        <Container>
+          <Heading lineLeft lineColor="secondary">
+            News
+          </Heading>
+          <GameCardSlider items={newGames} arrowColor="black" />
+        </Container>
+      </S.SectionNews>
+
+      <Container>
+        <S.SectionMostPopular>
+          <Heading lineLeft lineColor="secondary">
+            Most Popular
+          </Heading>
+          <Highlight {...mostPopularHighlight} />
+          <GameCardSlider items={mostPopularGames} />
+        </S.SectionMostPopular>
+
+        <S.SectionUpcomming>
+          <Heading lineLeft lineColor="secondary">
+            Upcomming
+          </Heading>
+          <GameCardSlider items={upcommingGames} />
+          <Highlight {...upcommingHighlight} />
+          <GameCardSlider items={upcommingMoreGames} />
+        </S.SectionUpcomming>
+
+        <S.SectionFreeGames>
+          <Heading lineLeft lineColor="secondary">
+            Free Games
+          </Heading>
+          <Highlight {...freeHighlight} />
+          <GameCardSlider items={freeGames} />
+        </S.SectionFreeGames>
+      </Container>
+
+      <S.SectionFooter>
+        <Container>
+          <Footer />
+        </Container>
+      </S.SectionFooter>
+    </section>
   );
 }
 
