1fe546650340cdee9140f8f67aa1634fe1ca75e9
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-15 15:36:43 -0300

    :wrench: config: config page home "/" for nextJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
-import Home from "@/templates/Home";
+import BannerSliderMock from "@/components/BannerSlider/BannerSlider.mock";
+import GameCardSliderMock from "@/components/GameCardSlider/GameCardSlider.mock";
+import HighlightMock from "@/components/Highlight/Highlight.mock";
 
-export default function Index() {
-  return <Home />;
+import Home, { HomeTamplateProps } from "@/templates/Home";
+
+export default function Index(props: HomeTamplateProps) {
+  return <Home {...props} />;
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      banners: BannerSliderMock,
+      newGames: GameCardSliderMock,
+      mostPopularHighlight: HighlightMock,
+      mostPopularGames: GameCardSliderMock,
+      upcommingHighlight: HighlightMock,
+      upcommingGames: GameCardSliderMock,
+      upcommingMoreGames: GameCardSliderMock,
+      freeHighlight: HighlightMock,
+      freeGames: GameCardSliderMock,
+    },
+  };
 }
