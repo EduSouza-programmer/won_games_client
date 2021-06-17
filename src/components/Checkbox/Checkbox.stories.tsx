63c7a0dc442063f9683c39d423352dcf96d9c8bd
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-17 10:41:58 -0300

    :pencil: docs: created storybook <Checkbox/>

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,9 +1,64 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import Checkbox from ".";
+import Checkbox, { CheckboxProps } from ".";
 
 export default {
   title: "Checkbox",
   component: Checkbox,
-} as Meta;
+  argTypes: {
+    labelColor: {
+      options: ["white", "black"],
+      control: { type: "radio" },
+    },
+    label: {
+      defaultValue: "TRY IT",
+    },
+    labelFor: {
+      defaultValue: "TRY IT",
+    },
+    onCheck: { action: "checked" },
+    value: {
+      control: false,
+    },
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+    layout: "fullscreen",
 
-export const Default: Story = () => <Checkbox />;
+    backgrounds: {
+      default: "won-dark",
+    },
+  },
+} as Meta<CheckboxProps>;
+
+export const Default: Story<CheckboxProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        width: "50%",
+        height: "10rem",
+      }}
+    >
+      <Checkbox label="Action" labelFor="action" isChecked />
+
+      <Checkbox label="Adventure" labelFor="adventure" />
+
+      <Checkbox label="Strategy" labelFor="strategy" />
+
+      <Checkbox {...args} />
+    </div>
+  </div>
+);
