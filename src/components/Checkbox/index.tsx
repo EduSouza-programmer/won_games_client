c97857676113bdfe338900daae251170dd68db5a
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-17 10:40:37 -0300

    :sparkles: feature: created the component <Checkbox/>

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,9 +1,50 @@
+import { useState, useCallback, InputHTMLAttributes } from "react";
 import * as S from "./Checkbox.styles";
 
-function Checkbox() {
+export type CheckboxProps = {
+  label?: string;
+  labelFor?: string;
+  labelColor?: "black" | "white";
+  onCheck?: (status: boolean) => void;
+  isChecked?: boolean;
+  value?: string | ReadonlyArray<string> | number;
+} & InputHTMLAttributes<HTMLInputElement>;
+
+function Checkbox({
+  label,
+  labelFor = "",
+  labelColor = "white",
+  onCheck,
+  isChecked = false,
+  value,
+  ...props
+}: CheckboxProps) {
+  const [checked, setChecked] = useState(isChecked);
+
+  const onChange = useCallback(() => {
+    const toogle = !checked;
+    setChecked(toogle);
+
+    if (onCheck) {
+      onCheck(toogle);
+    }
+  }, [onCheck, checked]);
+
   return (
     <S.Wrapper>
-      <h1>Checkbox</h1>
+      <S.Input
+        onChange={onChange}
+        id={labelFor}
+        type="checkbox"
+        checked={checked}
+        value={value}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
     </S.Wrapper>
   );
 }
