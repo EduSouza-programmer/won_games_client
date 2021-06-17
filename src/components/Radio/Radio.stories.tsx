2a3adb75a4813c1c2b02903d59c94dafb514a118
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-17 17:55:12 -0300

    :pencil: docs: created storybook the component <Radio/>

--- src/components/Radio/Radio.stories.tsx
+++ src/components/Radio/Radio.stories.tsx
@@ -1,9 +1,73 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import Radio from ".";
+import Radio, { RadioProps } from ".";
 
 export default {
   title: "Radio",
   component: Radio,
-} as Meta;
+  argTypes: {
+    labelColor: {
+      options: ["white", "black"],
+      control: { type: "radio" },
+    },
+    labelFor: {
+      control: false,
+    },
+    onCheck: { action: "checked" },
+    value: {
+      control: { type: "text" },
+    },
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+    backgrounds: {
+      default: "won-dark",
+    },
+    layout: "fullscreen",
+  },
+} as Meta<RadioProps>;
 
-export const Default: Story = () => <Radio />;
+export const Default: Story<RadioProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div
+      style={{
+        width: "50%",
+        height: "10rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+      }}
+    >
+      <Radio
+        name="radio"
+        label="Maior preço"
+        labelFor="maior preço"
+        value="primeiro"
+        defaultChecked
+      />
+      <Radio
+        name="radio"
+        label="Menor preço"
+        labelFor="menor preço"
+        value="segundo"
+      />
+      <Radio
+        name="radio"
+        label="TRY IT"
+        labelFor="try it"
+        value="try it"
+        {...args}
+      />
+    </div>
+  </div>
+);
