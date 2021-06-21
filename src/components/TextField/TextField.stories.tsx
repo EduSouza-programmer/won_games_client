d14e4c921137ea4ce0313932cd36d84c7c8c0898
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-21 14:18:57 -0300

    :pencil: docs: created storybook the component <TextField/>

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -1,9 +1,148 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import TextField from ".";
+import { Email } from "@styled-icons/material-outlined/Email";
+
+import TextField, { TextFieldProps } from ".";
 
 export default {
-  title: "TextField",
+  title: "Components/Forms/TextField",
   component: TextField,
-} as Meta;
+  argTypes: {
+    labelFor: {
+      control: { type: "null" },
+      defaultValue: "any",
+      table: {
+        category: "logics",
+      },
+    },
+    label: {
+      defaultValue: "E-mail",
+      table: {
+        category: "texts",
+      },
+    },
+    error: {
+      table: {
+        category: "texts",
+      },
+    },
+    icon: {
+      control: { type: "null" },
+      table: {
+        category: "logics",
+      },
+    },
+    iconPosition: {
+      options: ["left", "right"],
+      control: { type: "select" },
+      table: {
+        category: "choices",
+      },
+    },
+    onValue: {
+      action: "changes",
+      table: {
+        category: "logics",
+      },
+    },
+    placeholder: {
+      table: {
+        category: "texts",
+      },
+    },
+    disabled: {
+      table: {
+        category: "choices",
+      },
+    },
+  },
+  args: {
+    placeholder: "john.cage@gmail.com",
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+    layout: "fullscreen",
+  },
+} as Meta<TextFieldProps>;
 
-export const Default: Story = () => <TextField />;
+export const Default: Story<TextFieldProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div style={{ width: "25rem" }}>
+      <TextField {...args} />
+    </div>
+  </div>
+);
+
+export const WithIcon: Story<TextFieldProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div style={{ width: "27rem" }}>
+      <TextField {...args} />
+    </div>
+  </div>
+);
+
+WithIcon.args = {
+  icon: <Email />,
+};
+
+export const Error: Story<TextFieldProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div style={{ width: "27rem" }}>
+      <TextField {...args} />
+    </div>
+  </div>
+);
+
+Error.args = {
+  icon: <Email />,
+  error: "Ops... something is wrong",
+};
+
+export const Disabled: Story<TextFieldProps> = (args) => (
+  <div
+    style={{
+      width: "100%",
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <div style={{ width: "27rem" }}>
+      <TextField {...args} />
+    </div>
+  </div>
+);
+
+Disabled.args = {
+  disabled: true,
+};
