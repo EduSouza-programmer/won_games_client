cbddbe42abb4bbb827c7ef6dbd01afc9fdf33b53
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-25 21:15:57 -0300

    :white_check_mark: test: started the template page <Auth…

--- src/templates/Auth/Auth.test.tsx
+++ src/templates/Auth/Auth.test.tsx
@@ -4,11 +4,23 @@ import { renderWithTheme } from "@/utils/tests/helpers";
 import Auth from ".";
 
 describe("<Auth />", () => {
-  it("should render the heading", () => {
-    const { container } = renderWithTheme(<Auth />);
+  it("should render all components and children", () => {
+    renderWithTheme(
+      <Auth title="my title">
+        <input type="text" />
+      </Auth>
+    );
 
-    expect(screen.getByRole("heading", { name: /Auth/i })).toBeInTheDocument();
+    const logos = screen.getAllByRole("img", { name: /won games/i });
+    expect(logos).toHaveLength(2);
 
-    expect(container.firstChild).toMatchSnapshot();
+    const headings = screen.getAllByRole("heading");
+    expect(headings).toHaveLength(3);
+
+    const headingRight = screen.getByRole("heading", { name: /my title/i });
+    expect(headingRight).toBeInTheDocument();
+
+    const children = screen.getByRole("textbox");
+    expect(children).toBeInTheDocument();
   });
 });
