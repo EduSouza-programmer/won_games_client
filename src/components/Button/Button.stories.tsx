31c46dcc0d99759eeb53909ffee109bfb89b7449
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-28 16:47:54 -0300

    :pencil: docs: reactory storybook the component <Bu…

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,5 +1,7 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart";
+import { FavoriteBorder } from "@styled-icons/material-outlined/FavoriteBorder";
+import styled from "styled-components";
 
 import Button, { ButtonProps } from ".";
 
@@ -13,76 +15,136 @@ export default {
     layout: "fullscreen",
   },
   argTypes: {
-    children: {
-      type: "string",
-      defaultValue: "Buy now",
-    },
     size: {
+      description: "Multiple choices",
       options: ["small", "medium", "large"],
       control: { type: "select" },
-      defaultValue: "medium",
-    },
 
+      table: {
+        category: "choices",
+        type: {
+          summary: "small | medium | large",
+        },
+      },
+    },
+    fullWidth: {
+      description: "Boolean",
+      control: { type: "boolean" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "true | false",
+        },
+      },
+    },
     icon: {
-      control: false,
+      description: "Icon SVG",
+      control: { type: null },
+
+      table: {
+        category: "logics",
+        type: {
+          summary: "JSX.Element",
+        },
+      },
+    },
+    as: {
+      description: "Polymorphism styled-components",
+      control: { type: null },
+
+      table: {
+        category: "logics",
+        type: {
+          summary: "React.ElementType",
+        },
+      },
+    },
+    href: {
+      description: "Need for polymorphism styled-components",
+      control: { type: null },
+
+      table: {
+        category: "logics",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    minimal: {
+      description: "Boolean",
+      control: { type: "boolean" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "true | false",
+        },
+      },
     },
 
-    as: {
-      control: false,
+    resposiveSizes: {
+      description: "Object with sizes, mobile concepts first",
+      control: { type: "object" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "{small | medium | large}",
+        },
+      },
     },
+
+    children: {
+      description: "Prop children can be anything",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "React.reactNode",
+        },
+      },
+    },
+  },
+  args: {
+    children: "Buy now",
+    fullWidth: false,
+    minimal: false,
   },
 } as Meta;
 
+const WrapperCenter = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  height: 100vh;
+`;
+
 export const Default: Story<ButtonProps> = (args) => (
-  <div
-    style={{
-      maxWidth: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
+  <WrapperCenter>
     <Button {...args} />
-  </div>
+  </WrapperCenter>
 );
 
 export const WithIcon: Story<ButtonProps> = (args) => (
-  <div
-    style={{
-      maxWidth: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
+  <WrapperCenter>
     <Button {...args} />
-  </div>
+  </WrapperCenter>
 );
 
 WithIcon.args = {
-  children: "Buy Now",
   icon: <AddShoppingCart />,
 };
 
 export const AsLink: Story<ButtonProps> = (args) => (
-  <div
-    style={{
-      maxWidth: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
+  <WrapperCenter>
     <Button {...args} />
-  </div>
+  </WrapperCenter>
 );
 
 AsLink.args = {
   size: "large",
-  children: "Buy Now",
   as: "a",
   href: "/link",
 };
@@ -92,6 +154,34 @@ AsLink.argTypes = {
     control: false,
   },
   href: {
-    control: false,
+    control: { type: "text" },
   },
+};
+
+export const Minimal: Story<ButtonProps> = (args) => (
+  <WrapperCenter>
+    <Button {...args} />
+  </WrapperCenter>
+);
+
+Minimal.args = {
+  minimal: true,
+  children: "Lista de desejos",
+  icon: <FavoriteBorder />,
+};
+
+export const Responsive: Story<ButtonProps> = (args) => (
+  <WrapperCenter>
+    <Button {...args} />
+  </WrapperCenter>
+);
+
+Responsive.parameters = {
+  viewport: {
+    defaultViewport: "desktop",
+  },
+};
+
+Responsive.args = {
+  resposiveSizes: { medium: "medium", large: "large" },
 };
