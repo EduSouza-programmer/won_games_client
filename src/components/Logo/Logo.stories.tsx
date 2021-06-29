4461b39d5062fc61e1d2b3443762008040928ede
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-29 20:35:35 -0300

    :sparkles: feature: Finished organization storyboo…

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -2,20 +2,64 @@ import { Story, Meta } from "@storybook/react/types-6-0";
 import Logo, { LogoProps } from ".";
 
 export default {
-  title: "Logo",
+  title: "Components/Logo",
   component: Logo,
   argTypes: {
     color: {
+      description: "Logo text color",
       options: ["white", "black"],
       control: { type: "radio" },
-      defaultValue: "white",
+      table: {
+        category: "colors",
+        type: {
+          summary: "white | black",
+        },
+      },
     },
     size: {
+      description: "Logo size",
       options: ["normal", "large"],
       control: { type: "radio" },
-      defaultValue: "normal",
+      table: {
+        category: "choices",
+        type: {
+          summary: "normal | large",
+        },
+      },
+    },
+    hideOnMobile: {
+      description: "When true hides the logo text",
+      control: { type: "boolean" },
+      table: {
+        category: "choices",
+        type: {
+          summary: "boolean",
+          detail: "Hide the logo text",
+        },
+      },
+    },
+    id: {
+      description: "An id to make the logo unique",
+      control: { type: "null" },
+      table: {
+        category: "logics",
+        type: {
+          summary: "string",
+        },
+      },
     },
   },
-} as Meta;
+  args: {
+    color: "black",
+    size: "large",
+    hideOnMobile: false,
+  },
+  parameters: {
+    viewport: {
+      defaultViewport: "mobile1",
+    },
+    layout: "centered",
+  },
+} as Meta<LogoProps>;
 
 export const Default: Story<LogoProps> = (args) => <Logo {...args} />;
