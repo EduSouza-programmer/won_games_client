5cf287c5306784d1a2062d26183f5e28eb11a72a
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-29 13:19:15 -0300

    :lipstick: ui: created styles the component <FormS…

---
+++ src/components/FormSignUp/FormSignUp.styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+import * as Inherited from "@/components/FormSignIn/FormSignIn.styles";
+
+export const Wrapper = styled(Inherited.Wrapper)``;
+
+export const FormLink = styled(Inherited.FormLink)``;
