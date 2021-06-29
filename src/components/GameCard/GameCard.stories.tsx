b1833ffcce6253143ebb7d4b1b26dd4a6bd0281d
Eduardo Souza <dado.eletrons@gmail.com>
2021-06-29 14:05:18 -0300

    :pencil: docs: Refactory storybook, better organiz…

--- src/components/GameCard/GameCard.stories.tsx
+++ src/components/GameCard/GameCard.stories.tsx
@@ -1,53 +1,161 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
+import styled from "styled-components";
+
 import GameCard, { GameCardProps } from ".";
 
 export default {
-  title: "GameCard",
+  title: "Components/GameCard",
   component: GameCard,
   argTypes: {
-    promotionalPrice: {
+    title: {
+      type: { required: true },
+      description: "GameCard title",
       control: { type: "text" },
+
+      table: {
+        category: "texts",
+
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    developer: {
+      type: { required: true },
+      description: "GameCard developer game",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    img: {
+      type: { required: true },
+      description: "GameCard image",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    price: {
+      type: { required: true },
+      description: "GameCarde price game",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    promotionalPrice: {
+      description: "Discount price game",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
     },
     favorite: {
+      description: "Checkbox favorite icon for game",
       options: [true, false],
+      control: { type: "boolean" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "boolean",
+        },
+      },
+    },
+    onFav: {
+      description: "Get/Set value state",
+      control: { type: "null" },
+      action: "clicked",
+
+      table: {
+        category: "logics",
+        type: {
+          summary: "function",
+        },
+      },
     },
     ribbon: {
+      description: "Ribbon tag",
       control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "React.ReactNode",
+        },
+      },
     },
     ribbonColor: {
+      description: "Ribbon color",
       options: ["primary", "secondary"],
       control: { type: "radio" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "primary | secondary",
+        },
+      },
     },
     ribbonSize: {
+      description: "Ribbon size",
       options: ["normal", "small"],
       control: { type: "select" },
+
+      table: {
+        category: "choices",
+        type: {
+          summary: "normal | small",
+        },
+      },
     },
-    onFav: { action: "clicked" },
   },
   args: {
     title: "Population Zero",
     developer: "Other Ocean",
     img: "/img/population-zero.jpg",
     price: "R$ 235,00",
+    favorite: false,
   },
   parameters: {
     layout: "centered",
     backgrounds: {
-      default: "dark",
+      default: "won-dark",
     },
   },
 } as Meta<GameCardProps>;
 
+const WrapperCenter = styled.div`
+  width: 30rem;
+`;
+
 export const Default: Story<GameCardProps> = (args) => (
-  <div style={{ width: "30rem" }}>
+  <WrapperCenter>
     <GameCard {...args} />
-  </div>
+  </WrapperCenter>
 );
 
 export const PromotionalPrice: Story<GameCardProps> = (args) => (
-  <div style={{ width: "30rem" }}>
+  <WrapperCenter>
     <GameCard {...args} />
-  </div>
+  </WrapperCenter>
 );
 
 PromotionalPrice.args = {
