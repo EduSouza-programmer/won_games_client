04c9bc4cdd61c708d4f24d4c45786f12f0f6ca0d
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-02 10:47:12 -0300

    :pencil: docs: Organization storybook the compone…
    Closes #48

--- src/components/TextField/TextField.stories.tsx
+++ src/components/TextField/TextField.stories.tsx
@@ -7,119 +7,121 @@ export default {
   title: "Components/Forms/TextField",
   component: TextField,
   argTypes: {
+    label: {
+      description: "TextField label",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
     labelFor: {
+      description: "HTML for linking input label",
       control: { type: "null" },
-      defaultValue: "any",
+
       table: {
         category: "logics",
+        type: {
+          summary: "string",
+        },
       },
     },
-    label: {
-      defaultValue: "E-mail",
+    onValue: {
+      description: "Callback function that takes the input value as parameters",
+      control: { type: "null" },
+      action: "changes",
       table: {
-        category: "texts",
-      },
-    },
-    error: {
-      table: {
-        category: "texts",
+        category: "logics",
+        type: {
+          summary: "(value?: InputValue) => void",
+          detail: "onValue(inputRef.current?.value)",
+        },
       },
     },
     icon: {
+      description: "TextFiel Icon",
       control: { type: "null" },
+
       table: {
         category: "logics",
+        type: {
+          summary: "JSX.Element",
+        },
       },
     },
     iconPosition: {
+      description: "Icon position on input",
       options: ["left", "right"],
       control: { type: "select" },
       table: {
         category: "choices",
-      },
-    },
-    onValue: {
-      action: "changes",
-      table: {
-        category: "logics",
-      },
-    },
-    placeholder: {
-      table: {
-        category: "texts",
+        type: {
+          summary: "left | right",
+        },
       },
     },
     disabled: {
+      description: "Disabled-to-input behavior",
+      control: { type: "boolean" },
+
       table: {
         category: "choices",
+        type: {
+          summary: "boolean",
+          detail: "true | false",
+        },
+      },
+    },
+    error: {
+      description: "Input appearance in case of error",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    placeholder: {
+      description: "Input placeholder",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
       },
     },
   },
   args: {
+    label: "Email",
+    disabled: false,
     placeholder: "john.cage@gmail.com",
   },
   parameters: {
     viewport: {
-      defaultViewport: "mobile1",
+      defaultViewport: "figmaMobile",
     },
-    layout: "fullscreen",
+    layout: "centered",
   },
 } as Meta<TextFieldProps>;
 
-export const Default: Story<TextFieldProps> = (args) => (
-  <div
-    style={{
-      width: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <div style={{ width: "25rem" }}>
-      <TextField {...args} />
-    </div>
-  </div>
-);
+export const Default: Story<TextFieldProps> = (args) => <TextField {...args} />;
 
 export const WithIcon: Story<TextFieldProps> = (args) => (
-  <div
-    style={{
-      width: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <div style={{ width: "27rem" }}>
-      <TextField {...args} />
-    </div>
-  </div>
+  <TextField {...args} />
 );
 
 WithIcon.args = {
   icon: <Email />,
 };
 
-export const Error: Story<TextFieldProps> = (args) => (
-  <div
-    style={{
-      width: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <div style={{ width: "27rem" }}>
-      <TextField {...args} />
-    </div>
-  </div>
-);
+export const Error: Story<TextFieldProps> = (args) => <TextField {...args} />;
 
 Error.args = {
   icon: <Email />,
@@ -127,20 +129,7 @@ Error.args = {
 };
 
 export const Disabled: Story<TextFieldProps> = (args) => (
-  <div
-    style={{
-      width: "100%",
-      minHeight: "100vh",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <div style={{ width: "27rem" }}>
-      <TextField {...args} />
-    </div>
-  </div>
+  <TextField {...args} />
 );
 
 Disabled.args = {
