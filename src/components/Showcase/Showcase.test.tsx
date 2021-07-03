52df57e7812b708610cc4bf526b9b43162dc59a1
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-03 18:32:03 -0300

    :white_check_mark: test: Starting the component <S…

--- src/components/Showcase/Showcase.test.tsx
+++ src/components/Showcase/Showcase.test.tsx
@@ -1,16 +1,117 @@
+// eslint-disable-next-line import/no-unresolved
+import "match-media-mock";
+
 import { screen } from "@testing-library/react";
 import { renderWithTheme } from "@/utils/tests/helpers";
 
-import Showcase from ".";
+import highlightMock from "@/components/Highlight/Highlight.mock";
+import gameCardSliderMock from "@/components/GameCardSlider/GameCardSlider.mock";
+
+import Showcase, { ShowcaseProps } from ".";
+
+const props: ShowcaseProps = {
+  title: "my title",
+  highlight: highlightMock,
+  gamesSlider: gameCardSliderMock.slice(0, 1),
+};
 
 describe("<Showcase />", () => {
-  it("should render the heading", () => {
+  it("should render nothing if no prop is passed", () => {
     const { container } = renderWithTheme(<Showcase />);
 
+    expect(container.firstChild).toBeEmptyDOMElement();
+  });
+
+  it("should render a title when passed", () => {
+    const { container } = renderWithTheme(<Showcase title={props.title} />);
+
     expect(
-      screen.getByRole("heading", { name: /Showcase/i })
+      screen.getByRole("heading", { name: /my title/i })
     ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    const totalChildren = container.firstElementChild?.childElementCount;
+    expect(totalChildren).toBe(1);
+  });
+
+  it("should render without title", () => {
+    const { container } = renderWithTheme(
+      <Showcase gamesSlider={props.gamesSlider} highlight={highlightMock} />
+    );
+
+    screen.getByRole("heading", { name: highlightMock.title });
+    screen.getByRole("heading", { name: gameCardSliderMock[0].title });
+
+    expect(
+      screen.queryByRole("heading", { name: /my title/i })
+    ).not.toBeInTheDocument();
+
+    const totalChildren = container.firstElementChild?.childElementCount;
+    expect(totalChildren).toBe(2);
+  });
+
+  it("should render a highlight when passed", () => {
+    const { container } = renderWithTheme(
+      <Showcase highlight={props.highlight} />
+    );
+
+    expect(
+      screen.getByRole("heading", { name: highlightMock.title })
+    ).toBeInTheDocument();
+
+    const totalChildren = container.firstElementChild?.childElementCount;
+    expect(totalChildren).toBe(1);
+  });
+
+  it("should render without highlight", () => {
+    const { container } = renderWithTheme(
+      <Showcase title={props.title} gamesSlider={props.gamesSlider} />
+    );
+
+    screen.getByRole("heading", { name: props.title });
+    screen.getByRole("heading", { name: gameCardSliderMock[0].title });
+
+    expect(
+      screen.queryByRole("heading", { name: highlightMock.title })
+    ).not.toBeInTheDocument();
+
+    const totalChildren = container.firstElementChild?.childElementCount;
+    expect(totalChildren).toBe(2);
+  });
+
+  it("should render a gamesSlider when passed", () => {
+    const { container } = renderWithTheme(
+      <Showcase gamesSlider={props.gamesSlider} />
+    );
+
+    expect(
+      screen.getAllByRole("heading", { name: gameCardSliderMock[0].title })[0]
+    ).toBeInTheDocument();
+
+    const totalChildren = container.firstElementChild?.childElementCount;
+
+    expect(totalChildren).toBe(1);
+  });
+
+  it("should render without gamesSlider", () => {
+    const { container } = renderWithTheme(
+      <Showcase title={props.title} highlight={props.highlight} />
+    );
+
+    screen.getByRole("heading", { name: props.title });
+    screen.getByRole("heading", { name: highlightMock.title });
+
+    expect(
+      screen.queryByRole("heading", { name: gameCardSliderMock[0].title })
+    ).not.toBeInTheDocument();
+
+    const totalChildren = container.firstElementChild?.childElementCount;
+    expect(totalChildren).toBe(2);
+  });
+
+  it("should render correct count of all elements", () => {
+    const { container } = renderWithTheme(<Showcase {...props} />);
+    const totalChildren = container.firstElementChild?.childElementCount;
+
+    expect(totalChildren).toBe(3);
   });
 });
