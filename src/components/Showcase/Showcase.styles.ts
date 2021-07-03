1d0c13f5e00467ac3c6c1423d6e061db425b25af
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-03 18:41:34 -0300

    :lipstick: ui: Created styles the component <Showc…

--- src/components/Showcase/Showcase.styles.ts
+++ src/components/Showcase/Showcase.styles.ts
@@ -1,7 +1,30 @@
 import styled, { css } from "styled-components";
+import media from "styled-media-query";
 
-export const Wrapper = styled.div`
+import { Container } from "@/components/Container";
+import * as HeadingStyles from "@/components/Heading/Heading.styles";
+import * as HighlightStyles from "@/components/Highlight/Highlight.styles";
+import * as GameCardSliderStyles from "@/components/GameCardSlider/GameCardSlider.styles";
+
+export const Wrapper = styled(Container).attrs({ as: "div" })`
   ${({ theme }) => css`
-    background-color: ${theme.palette.primary.main};
+    ${HeadingStyles.Wrapper},
+    ${HighlightStyles.Wrapper},
+    ${GameCardSliderStyles.Wrapper} {
+      margin-bottom: ${theme.spacings.medium};
+    }
+
+    ${HighlightStyles.Wrapper} {
+      ${media.lessThan("medium")`
+        margin-right: calc((${theme.gridLayout.gutter} / 2) * -1);
+        margin-left: calc((${theme.gridLayout.gutter} / 2) * -1);
+      `}
+    }
+
+    ${GameCardSliderStyles.Wrapper} {
+      ${media.lessThan("huge")`
+        margin-right: calc((${theme.gridLayout.gutter} / 2) * -1);
+      `}
+    }
   `}
 `;
