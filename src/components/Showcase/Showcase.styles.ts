0ca96b8bbe3dc15aecff8bada36fda6a4e55f3cb
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-05 20:25:52 -0300

    :lipstick: ui: Modification of the component h…

--- src/components/Showcase/Showcase.styles.ts
+++ src/components/Showcase/Showcase.styles.ts
@@ -2,15 +2,13 @@ import styled, { css } from "styled-components";
 import media from "styled-media-query";
 
 import { Container } from "@/components/Container";
-import * as HeadingStyles from "@/components/Heading/Heading.styles";
 import * as HighlightStyles from "@/components/Highlight/Highlight.styles";
 import * as GameCardSliderStyles from "@/components/GameCardSlider/GameCardSlider.styles";
 
 export const Wrapper = styled(Container).attrs({ as: "div" })`
   ${({ theme }) => css`
-    ${HeadingStyles.Wrapper},
-    ${HighlightStyles.Wrapper},
-    ${GameCardSliderStyles.Wrapper} {
+    .heading,
+    ${HighlightStyles.Wrapper}, ${GameCardSliderStyles.Wrapper} {
       margin-bottom: ${theme.spacings.medium};
     }
 
