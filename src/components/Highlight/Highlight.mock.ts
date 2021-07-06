b6a5f30361dab58a804da02f72d47bb8d1b5be5e
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-06 11:05:51 -0300

    :clown_face: clown-face: Adding mocks for compon…

--- src/components/Highlight/Highlight.mock.ts
+++ src/components/Highlight/Highlight.mock.ts
@@ -1,9 +1,28 @@
 import { HighlightProps } from ".";
 
 export default {
-  title: "Read Dead it's back",
-  subtitle: "Come see John's new adventures",
+  title: "Read Dead está de volta!",
+  subtitle: "Venha conhecer as novas aventuras de John Marston.",
   buttonLabel: "Buy now",
   buttonLink: "https://any",
-  backgroundImage: "/img/red-dead-img.jpg",
+  backgroundImage: "/img/highlight-back-red-dead.jpg",
+  floatImage: "/img/highlight-float-red-dead.png",
+} as HighlightProps;
+
+export const highlightExample2 = {
+  title: "Borderlands 3!",
+  subtitle: "Borderlands está de volta com muitas novidades.",
+  buttonLabel: "Buy now",
+  buttonLink: "https://any",
+  backgroundImage: "/img/highlight-back-borderlands.jpg",
+  floatImage: "/img/highlight-float-borderlands.png",
+} as HighlightProps;
+
+export const highlightExample3 = {
+  title: "Já conhece o clássico CS:GO?",
+  subtitle: "Jogue um dos maiores clássicos do FPS.",
+  buttonLabel: "Buy now",
+  buttonLink: "https://any",
+  backgroundImage: "/img/highlight-back-csgo.jpg",
+  floatImage: "/img/highlight-float-csgo.png",
 } as HighlightProps;
