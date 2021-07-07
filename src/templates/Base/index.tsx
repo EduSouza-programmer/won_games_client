0f6c560a0d8cbd3e1dd1ce6f4846c0873ac197b9
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-07 13:13:20 -0300

    :sparkles: feature: Finished <Base/>
    Finished template the page <Base/>.

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -1,14 +1,26 @@
+import { Container } from "@/components/Container";
+import Footer from "@/components/Footer";
+import Menu from "@/components/Menu";
+
 import * as S from "./Base.styles";
 
 export type BaseProps = {
-  example: string;
+  children: React.ReactNode;
 };
 
-function Base() {
+function Base({ children }: BaseProps) {
   return (
-    <S.Wrapper>
-      <h1>Base</h1>
-    </S.Wrapper>
+    <section>
+      <Container>
+        <Menu />
+      </Container>
+      {children}
+      <S.SectionFooter>
+        <Container>
+          <Footer />
+        </Container>
+      </S.SectionFooter>
+    </section>
   );
 }
 
