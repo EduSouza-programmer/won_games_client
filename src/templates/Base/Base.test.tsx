3c2a7481b7e44b7adbc1eaf054425db6b9505f20
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-07 13:11:25 -0300

    :white_check_mark: test: Finished <Base/>
    Finished template page <Base/> with TDD.

--- src/templates/Base/Base.test.tsx
+++ src/templates/Base/Base.test.tsx
@@ -3,12 +3,34 @@ import { renderWithTheme } from "@/utils/tests/helpers";
 
 import Base from ".";
 
+jest.mock("@/components/Menu", () => {
+  return {
+    __esModule: true,
+    default: () => <div data-testid="Mock Menu" />,
+  };
+});
+
+jest.mock("@/components/Footer", () => {
+  return {
+    __esModule: true,
+    default: function myMock() {
+      return <div data-testid="Mock Footer" />;
+    },
+  };
+});
+
 describe("<Base />", () => {
-  it("should render the heading", () => {
-    const { container } = renderWithTheme(<Base />);
+  it("should render menu, footer an children", () => {
+    renderWithTheme(
+      <Base>
+        <h1>Heading</h1>
+      </Base>
+    );
 
-    expect(screen.getByRole("heading", { name: /Base/i })).toBeInTheDocument();
-
-    expect(container.firstChild).toMatchSnapshot();
+    expect(screen.getByTestId(/mock menu/i)).toBeInTheDocument();
+    expect(screen.getByTestId(/mock footer/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /heading/i })
+    ).toBeInTheDocument();
   });
 });
