3f893b6c365ab6cdca73040ce674c4c09825a132
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-09 14:42:14 -0300

    :lipstick: ui: Finished styles <GameInfo/>
    Created styles the component.

--- src/components/GameInfo/GameInfo.styles.ts
+++ src/components/GameInfo/GameInfo.styles.ts
@@ -1,7 +1,91 @@
 import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
+import Heading from "@/components/Heading";
+
+export const Description = Heading;
+
+export const Wrapper = styled.main`
   ${({ theme }) => css`
-    background-color: ${theme.palette.primary.main};
+    position: relative;
+    background-color: ${theme.palette.white.main};
+    padding: ${theme.spacings.large} ${theme.spacings.xsmall}
+      ${theme.spacings.medium};
+    margin: 0 -${theme.spacings.xsmall};
+
+    ${theme.media.greaterThan("xsmall")`
+
+      padding: ${theme.spacings.large} ${theme.spacings.small}
+      ${theme.spacings.medium};
+      margin: 0;
+
+    `}
+
+    .description {
+      margin-bottom: ${theme.spacings.small};
+    }
+
+    ${theme.media.greaterThan("small")`
+
+        padding: ${theme.spacings.medium} ${theme.spacings.small}
+
+    `}
+  `}
+`;
+
+export const Container = styled.div`
+  ${({ theme }) => css`
+    .heading {
+      margin-top: calc(${theme.spacings.xxsmall} + 2px);
+      margin-bottom: calc(${theme.spacings.xsmall} + 2px);
+    }
+
+    .price {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin-top: 1.6rem;
+
+      ${theme.media.greaterThan("xsmall")`
+        right: -14px;
+
+      `}
+    }
+
+    ${theme.media.greaterThan("small")`
+      display: flex;
+      justify-content: space-between;
+
+        .price {
+        position: static;
+        align-self: flex-start;
+      }
+
+    `}
+  `}
+`;
+
+export const WrapperButton = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+
+    > :first-child {
+      margin-bottom: ${theme.spacings.xxsmall};
+    }
+
+    /* display: grid; */
+    ${theme.media.greaterThan("small")`
+
+    flex-direction: row-reverse;
+    justify-content: flex-start;
+
+    /* grid-template-columns: auto auto;
+    justify-content: end; */
+
+    > :first-child {
+      margin-bottom: 0;
+    }
+
+    `}
   `}
 `;
