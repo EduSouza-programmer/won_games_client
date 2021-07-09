e4825660b28e64b4d8767bf56ba4eabc7df07413
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-09 14:42:56 -0300

    :pencil: docs: Created storybook <GameInfo/>
    Created storybook the component.

--- src/components/GameInfo/GameInfo.stories.tsx
+++ src/components/GameInfo/GameInfo.stories.tsx
@@ -1,14 +1,19 @@
 import { Story, Meta } from "@storybook/react/types-6-0";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { StoryFnReactReturnType } from "@storybook/react/dist/ts3.9/client/preview/types";
 
 import GameInfo, { GameInfoProps } from ".";
+import GameInfoMock from "./GameInfo.mock";
 
 const WrapperCenter = styled.div`
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  height: 100vh;
+  ${({ theme }) => css`
+    display: grid;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 120rem;
+    height: 100vh;
+    padding: 0 ${theme.spacings.xsmall};
+  `}
 `;
 
 const defaultDecorator = (Story: Story): StoryFnReactReturnType => (
@@ -21,32 +26,66 @@ export default {
   title: "GameInfo",
   component: GameInfo,
   argTypes: {
-    example: {
-      description: "",
-      control: { type: "" },
-      options: ["small", "medium", "large"],
+    title: {
+      description: "GameInfo title",
+      control: { type: "text" },
 
       table: {
-        category: "choices",
+        category: "texts",
         type: {
-          summary: "small | medium | large",
-          detail: "",
+          summary: "string",
+        },
+      },
+    },
+    description: {
+      description: "GameInfo description",
+      control: { type: "text" },
+
+      table: {
+        category: "texts",
+        type: {
+          summary: "string",
+        },
+      },
+    },
+    gamePrice: {
+      description: "GameInfo price",
+      control: { type: "object" },
+
+      table: {
+        category: "objects",
+        type: {
+          summary: "PriceProps",
+          detail: `(alias) type PriceProps = {
+            price: string;
+            promotionalPrice?: string | undefined;
+        }`,
         },
       },
     },
   },
-  args: {},
+  args: { ...GameInfoMock },
   parameters: {
     viewport: {
-      defaultViewport: "mobile1",
+      defaultViewport: "desktop",
     },
     backgrounds: {
-      default: "light",
+      default: "won-dark",
     },
     layout: "fullscreen",
-    controls: { hideNoControlsWarning: true },
   },
   decorators: [defaultDecorator],
 } as Meta<GameInfoProps>;
 
-export const Default: Story<GameInfoProps> = () => <GameInfo />;
+export const Default: Story<GameInfoProps> = (args) => <GameInfo {...args} />;
+
+export const WithPriceDiscount: Story<GameInfoProps> = (args) => (
+  <GameInfo {...args} />
+);
+
+WithPriceDiscount.args = {
+  gamePrice: {
+    price: "R$ 235,00",
+    promotionalPrice: "R$ 199,00",
+  },
+};
