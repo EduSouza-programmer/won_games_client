c52a2d64ca6c232de38ed41c1b4ecafc8eeeb44b
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-09 14:41:20 -0300

    :sparkles: feature: Finished <GameInfo/>
    Created the component <GameInfo/> with TDD.

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,13 +1,40 @@
+import { AddShoppingCart } from "@styled-icons/material-outlined/AddShoppingCart";
+import { FavoriteBorder } from "@styled-icons/material-outlined/FavoriteBorder";
+
+import Heading from "@/components/Heading";
+import Price, { PriceProps } from "@/components/Price";
+import Button from "@/components/Button";
 import * as S from "./GameInfo.styles";
 
 export type GameInfoProps = {
-  example: string;
+  title: string;
+  description: string;
+  gamePrice: PriceProps;
 };
 
-function GameInfo() {
+function GameInfo({ title, description, gamePrice }: GameInfoProps) {
   return (
     <S.Wrapper>
-      <h1>GameInfo</h1>
+      <S.Container>
+        <Heading className="heading" lineBottom size="medium" color="black">
+          {title}
+        </Heading>
+        <Price className="price" {...gamePrice} />
+      </S.Container>
+      <S.Description
+        responsiveSize={{ minFontSizes: 14, maxFontSizes: 18 }}
+        color="black"
+        as="p"
+        className="description"
+      >
+        {description}
+      </S.Description>
+      <S.WrapperButton>
+        <Button icon={<AddShoppingCart />}>Adicionar</Button>
+        <Button href="#" minimal as="a" icon={<FavoriteBorder />}>
+          Lista de desejos
+        </Button>
+      </S.WrapperButton>
     </S.Wrapper>
   );
 }
