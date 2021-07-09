9fece490070c27af4cd84170303c7da29de0f1c6
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-09 14:43:47 -0300

    :clown_face: clown-face: Created mock <GameInfo/>

---
+++ src/components/GameInfo/GameInfo.mock.ts
@@ -0,0 +1,10 @@
+import { GameInfoProps } from ".";
+
+export default {
+  title: "Borderlands 3",
+  description:
+    "Agora é a hora de eliminar Handsome Jack. Reúna seus amigos para correr desenfreadamente pelo mundo coletando milhões de iten.",
+  gamePrice: {
+    price: "R$ 235,00",
+  },
+} as GameInfoProps;
