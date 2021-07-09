688cb3c27912c893c0376275e544003aa5036386
Eduardo Souza <dado.eletrons@gmail.com>
2021-07-09 14:40:05 -0300

    :white_check_mark: test: Created tests <GameInfo/>
    Starting the component <GameInfo/> with TDD.

--- src/components/GameInfo/GameInfo.test.tsx
+++ src/components/GameInfo/GameInfo.test.tsx
@@ -1,16 +1,35 @@
 import { screen } from "@testing-library/react";
 import { renderWithTheme } from "@/utils/tests/helpers";
 
-import GameInfo from ".";
+import GameInfo, { GameInfoProps } from ".";
+
+const props: GameInfoProps = {
+  title: "My game title",
+  description: "Game description",
+  gamePrice: {
+    price: "R$ 210,00",
+  },
+};
 
 describe("<GameInfo />", () => {
-  it("should render the heading", () => {
-    const { container } = renderWithTheme(<GameInfo />);
+  it("should render game informations", () => {
+    renderWithTheme(<GameInfo {...props} />);
 
     expect(
-      screen.getByRole("heading", { name: /GameInfo/i })
+      screen.getByRole("heading", { name: /my game title/i })
+    ).toBeInTheDocument();
+    expect(screen.getByText(/game description/i)).toBeInTheDocument();
+    expect(screen.getByText(/210.00/i)).toBeInTheDocument();
+  });
+  it("should render buttons", () => {
+    renderWithTheme(<GameInfo {...props} />);
+
+    expect(
+      screen.getByRole("button", { name: /adicionar/i })
     ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot();
+    expect(
+      screen.getByRole("link", { name: /lista de desejos/i })
+    ).toBeInTheDocument();
   });
 });
